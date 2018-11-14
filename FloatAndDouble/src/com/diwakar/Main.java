5fe91688316abefb48c9f69ce9125287bcb9c59f
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-11-14 17:54:04 +1000

    FloatAndDouble

---
+++ FloatAndDouble/src/com/diwakar/Main.java
@@ -0,0 +1,33 @@
+package com.diwakar;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        //Width of int = 32 (4 bytes)
+//	    int myIntValue = 5/3;
+//
+//	    //Width of float = 32 (4 bytes)
+//	    float myFloatValue = 5f/3f;
+//
+//	    //Width of double = 64 (8 bytes)
+//	    double myDoubleValue = 5d/3d;
+//
+//        System.out.println("myIntValue = "+myIntValue);
+//        System.out.println("myFloatValue = "+myFloatValue);
+//        System.out.println("myDoubleValue = "+myDoubleValue);
+
+//        ***************************************************************************
+        // Convert a given number of pounds to kilograms
+        // 1. Create a variable to store the number of pounds
+        // 2. Calculate the number of Kilograms for the number above and store in a variable.
+        // 3. Print out the result.
+        //
+        // NOTES: 1 pound is equal to 0.45359237 kilograms.
+        //***************************************************************************
+
+        double pound = 10d;
+        double kilogram = pound * 0.45359237d;
+        System.out.println(kilogram);
+        
+    }
+}
