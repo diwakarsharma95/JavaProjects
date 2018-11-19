131ac2ab4602b51c27ce373dd4a12de0eef60ff6
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-11-19 19:02:28 +1000

    Moved files into session 1

---
+++ Session 1/Strings/src/com/diwakar/Main.java
@@ -0,0 +1,11 @@
+package com.diwakar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString = "This is my String";
+        System.out.println("Some text added to "+myString);
+        myString = myString + " Some more text added";
+        System.out.println(myString);
+    }
+}
