084541004608963c6459918276d0cca8080a7faf
Diwakar Sharma <diwakarsharma95@gmail.com>
2018-12-21 20:21:28 +1000

    methods

--- Session2/methods/src/com/diwakar/Main.java
+++ Session2/methods/src/com/diwakar/Main.java
@@ -4,20 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        calculateScore(true,800,5,100);
-
-        calculateScore(true,10000,8,200);
+//        int highScore = calculateScore(true,800,5,100);
+//        System.out.println("Your final score was "+highScore);
+//
+//        highScore = calculateScore(true,10000,8,200);
+//        System.out.println("Your final score was "+highScore);
+        displayHighScorePosition("Diwakar",calculateHighScorePosition(400));
 
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         if(gameOver){
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
-            System.out.println("Your final score was "+finalScore);
+            finalScore += 2000;
+
             return finalScore;
         }
 
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position){
+        System.out.println(playerName+" managed to get into "+position+" position");
+    }
+    public static int calculateHighScorePosition(int playerScore){
+        if (playerScore > 1000){
+            return 1;
+        }else if(500 < playerScore && playerScore < 1000){
+            return 2;
+        }else{
+            return 3;
+        }
     }
 }
